3fa03b3051edac546ceb481858b00d36a97e2147
Kiran Anand <liteninkiran@hotmail.com>
2021-07-25 03:21:02 +0100

    Adding navbar component

---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
